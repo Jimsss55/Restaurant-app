ad2c2a8f709eb2e49205d106368f60236a19f771
Jimpa <jamtshojimpa@gmail.com>
2025-01-09 15:23:50 +0600

    Add session

--- app/models/customer_table.rb
+++ app/models/customer_table.rb
@@ -1,4 +1,4 @@
 class CustomerTable < ApplicationRecord
-  validates :customer_table_number, presence: true, numericality: {
+  validates :customer_table_number, uniqueness: true, presence: true, numericality: {
     greater_than_or_equal_to: 1 }
 end
