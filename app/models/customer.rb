cb33783eb17017f33325d6ce2f51c2e0849ab156
Jimpa <jamtshojimpa@gmail.com>
2025-01-21 15:16:46 +0600

    Add Customer Join Table

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer < ApplicationRecord
-  belongs_to :customer_table
-  # has_and_belongs_to_many :customer_tables, dependent: :destroy
+  belongs_to :customer_table, optional: true
+  has_and_belongs_to_many :customer_tables
   has_many :order_items, dependent: :destroy
   has_many :menu_items, through: :order_items
   has_one :payment_detail, dependent: :destroy
--- app/models/customer_table.rb
+++ app/models/customer_table.rb
@@ -1,6 +1,6 @@
 class CustomerTable < ApplicationRecord
   has_many :customers
-  # has_and_belongs_to_many :customers
+  has_and_belongs_to_many :customers
 
   validates :customer_table_number, uniqueness: true, presence: true, numericality: {
     greater_than_or_equal_to: 1 }
