02ce478adc076710b0910d831aae03f6a6416580
Jimpa <jamtshojimpa@gmail.com>
2025-01-21 12:15:03 +0600

    Add Customer payment method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ApplicationRecord
   belongs_to :customer_table
+  # has_and_belongs_to_many :customer_tables, dependent: :destroy
   has_many :order_items, dependent: :destroy
   has_many :menu_items, through: :order_items
   has_one :payment_detail, dependent: :destroy
