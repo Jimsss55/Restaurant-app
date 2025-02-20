cc3c0f440af941101df396c2cbdafe5c595a9388
Jimpa <jamtshojimpa@gmail.com>
2025-02-20 12:58:02 +0600

    Updated email validation

--- app/models/payment_detail.rb
+++ app/models/payment_detail.rb
@@ -1,7 +1,7 @@
 class PaymentDetail < ApplicationRecord
   belongs_to :customer
   validates :email, presence: true,
-            format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
+            format: { with: /\A[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/}
 
   validates :journal_number, uniqueness: true, if: -> { journal_number.present? }
 
